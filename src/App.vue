366ff65ac9b12c0a73dd6343c53a367a0e50b6ab
Mikkel Beck Knudsen <mikkel1a2@gmail.com>
2020-11-04 12:25:09 +0100

    added project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+     <Navbar/>
+    <router-view />
+    <Footer/>
+</template>
+<script>
+import Navbar from "../src/components/Navbar"
+import Footer from "../src/components/Footer"
+
+export default {
+  components:{
+    Navbar,
+    Footer
+  }
+}
+</script>
+<style lang="scss">
+#app {
+  font-family: Arial, sans-serif, Avenir, Helvetica;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #212121;
+  
+}
+body{
+  margin: 0;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <h1>This is home page</h1>
+    <img alt="Vue logo" src="../assets/images/logo-wedio-black.svg" class="wedio-logo"/>
+    <Content msg="Welcome to Your Wedio Internship Test" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Content from "@/components/Content.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Content
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.home{
+  margin-top: 80px;
+}
+.wedio-logo{
+  max-width: 800px;
+  max-height: 400px;
+}
+h1{
+  margin-top: 100px ;
+}
+</style>
