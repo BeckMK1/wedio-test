fbebf9d441f45bee1ee904358c972ff8646c3f21
Mikkel Beck Knudsen <mikkel1a2@gmail.com>
2020-11-05 17:18:33 +0100

    modal created
    todo: add toggel botton, edit content, close on unfocus

---
+++ src/components/Modal.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="modal">
+        <header>
+            <p>this is a header</p>
+        </header>
+        <section>
+            <p>content</p>
+        </section>
+        <div>
+            <p>this is a footer</p>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+
+.modal{
+    width: 500px;
+    background: red;
+
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,17 +3,27 @@
     <h1>This is home page</h1>
     <img alt="Vue logo" src="../assets/images/logo-wedio-black.svg" class="wedio-logo"/>
     <Content msg="Welcome to Your Wedio Internship Test" />
+    <div class="modal-content" v-if="!isHidden">
+      <Modal></Modal>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Content from "@/components/Content.vue";
+import Modal from "@/components/Modal.vue";
 
 export default {
   name: "Home",
   components: {
-    Content
+    Content,
+    Modal
+  },
+  data() {
+    return{
+      isHidden: false
+    }
   }
 };
 </script>
@@ -28,5 +38,10 @@ export default {
 }
 h1{
   margin-top: 100px ;
+}
+.modal-content{
+left: calc(50% - 250px);
+bottom: 70%;
+position: absolute;
 }
 </style>
